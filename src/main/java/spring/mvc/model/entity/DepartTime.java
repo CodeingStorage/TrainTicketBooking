97c669af378c2e044bbc28a1323967e62b014596
Aaron <srimp12336691@gmail.com>
2024-01-11 15:13:28 +0800

    entity setup wip

---
+++ src/main/java/spring/mvc/model/entity/DepartTime.java
@@ -0,0 +1,7 @@
+package spring.mvc.model.entity;
+
+import java.util.Date;
+
+public class DepartTime {
+	private Date departTime;
+}
--- src/main/java/spring/mvc/model/entity/User.java
+++ src/main/java/spring/mvc/model/entity/User.java
@@ -1,5 +1,14 @@
 package spring.mvc.model.entity;
 
+/*
+ * 使用者資料
++-------------------+-----------------
+|       userId      |    ticketId    | 
++-------------------+-----------------
+|  	  A123456789    |     01234567   | 
+| 	  J98765432     | 	  98765432   | 
++-------------------+-----------------
+ */
 public class User {
 	private String userId;
 	private Integer ticketId;
