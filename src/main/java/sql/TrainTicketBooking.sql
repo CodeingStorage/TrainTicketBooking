1da55a337f3067b8a013b53c39da96f1c9d80450
DESKTOP-0I2LC28\Administrator <srimp12336691@gmail.com>
2024-01-14 21:44:56 +0800

    DB VOL2

--- src/main/java/sql/TrainTicketBooking.sql
+++ src/main/java/sql/TrainTicketBooking.sql
@@ -5,6 +5,8 @@ drop table if exists trainnorthbound;
 drop table if exists traintablesouthbound;
 drop table if exists trainsouthbound;
 drop table if exists ticket;
+drop table if exists traintable;
+drop table if exists direction;
 drop table if exists user;
 
 -- 建立用戶資訊 (身分證字號)
@@ -23,33 +25,21 @@ stationName varchar(20)-- 站點名稱
 alter table station auto_increment = 01;
 
 
--- 建立火車(南下)
-create table if not exists trainsouthbound(
-trainIdS int primary key
+-- 建立火車行車方向
+create table if not exists direction(
+directionId varchar(5) primary key,
+directionName varchar(10)
 );
 
--- 建立火車時刻表(南下)
-create table if not exists traintablesouthbound(
-trainIdS int not null,  -- 火車代號(4碼
+
+
+-- 建立火車時刻表
+create table if not exists traintable(
+trainId int not null,  -- 火車代號(4碼
+directionId varchar(5),
 station01departTime TIME,-- 台北站出發時間
 station02departTime TIME,-- 台中站出發時間
-station03departTime TIME,-- 高雄站出發時間
-foreign key (trainIdS) references trainsouthbound (trainIdS)
-);
-
-
--- 建立火車(北上)
-create table if not exists trainnorthbound(
-trainIdN int primary key
-);
-
--- 建立火車時刻表(北上)
-create table if not exists traintablenorthbound(
-trainIdN int not null, -- 火車代號(4碼)
-station03departTime TIME,-- 高雄站出發時間
-station02departTime TIME,-- 台中站出發時間
-station01departTime TIME,-- 台北站出發時間
-foreign key (trainIdN) references trainnorthbound (trainIdN)
+station03departTime TIME-- 高雄站出發時間
 );
 
 
@@ -75,7 +65,6 @@ foreign key (userId) references user (userId)
 
 
 
-
 -- 預設資料
 
 -- user 用戶資訊
@@ -88,19 +77,13 @@ INSERT INTO station(stationId,stationName) values(01,'台北');
 INSERT INTO station(stationId,stationName) values(02,'台中');
 INSERT INTO station(stationId,stationName) values(03,'高雄');
 
+-- direction行車方向
+INSERT INTO direction(directionId,directionName) values('N','北上');
+INSERT INTO direction(directionId,directionName) values('S','南下');
 
--- trainsouthbound 火車代碼(南下)
-INSERT INTO trainsouthbound(trainIdS) values(0001);
-
--- traintablesouthbound 時刻表(南下)
-INSERT INTO traintablesouthbound(trainIdS,station01departTime,station02departTime,station03departTime) values(0001,'9:00','10:00','11:00');
-
--- trainnorthbound 火車代碼(北上)
-INSERT INTO trainnorthbound(trainIdN) values(1001);
-
--- traintablenorthbound 時刻表(北上)
-INSERT INTO traintablenorthbound(trainIdN,station03departTime,station02departTime,station01departTime) values(1001,'12:00','13:00','14:00');
-
+-- traintable火車時刻表
+INSERT INTO traintable ( trainId, directionId, station01departTime, station02departTime, station03departTime ) VALUES ( 1001, 'S', '08:00:00', '10:00:00', '12:00:00' );
+INSERT INTO traintable ( trainId, directionId, station01departTime, station02departTime, station03departTime ) VALUES ( 1002, 'N', '15:00:00', '14:00:00', '13:00:00' );
 
 -- ticket 車票資訊(南下)
 INSERT INTO ticket ( ticketId, userId, trainId, date, departureTime, arrivalTime, startStationId, startStationName, endStationId, endStationName, trainCarId, seatId, price, bookTime ) VALUES (
