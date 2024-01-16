8b9264bd6615643029c8b58efb2f40d222507f08
Aaron <srimp12336691@gmail.com>
2024-01-16 15:33:14 +0800

    DAO wip

--- src/main/java/spring/mvc/model/dao/TrainTicketDAO.java
+++ src/main/java/spring/mvc/model/dao/TrainTicketDAO.java
@@ -3,33 +3,37 @@ package spring.mvc.model.dao;
 import java.util.List;
 import java.util.Optional;
 
+import spring.mvc.model.entity.Direction;
+import spring.mvc.model.entity.Station;
 import spring.mvc.model.entity.User;
 
 
 
 public interface TrainTicketDAO {
 //	使用者-User:
-//	1. 查詢所有使用者(多筆)
-	List<User> findAllUsers();
+//	1. 查詢所有使用者名稱(多筆)
+	List<User> findAllUserNames();
 	
 //	2. 新增使用者
 	void addUser(User user);
 
 //	5. 根據使用者ID查找使用者(單筆)
-	Optional<User> findUserById(Integer userId);
+	Optional<User> findUserByUserId(Integer userId);
 	
 
 //	站點-Station:	
 	
 //1. 根據站點ID查找站點名稱
+	Optional<Station> findStationNameByStationId(Integer stationId);
 	
 //2. 根據站點ID修改站點名稱
-	
+	public Boolean updateStationName(Integer stationId, String newStationName);
 	
 	
 //  行車方向-direction:
-//1. 根據行車方向ID查找行車方向名稱	
 	
+//1. 根據行車方向ID查找行車方向名稱	
+	Optional<Direction> findDirectionNameByDirectionId(Integer userId);
 //  火車車次-Train:
 //1.  新增車次ID(後台)
 //2. 根據車次ID修改車次(後台)
