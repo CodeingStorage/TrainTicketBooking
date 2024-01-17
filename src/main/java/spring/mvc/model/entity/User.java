eaf4d69a67c8f280fbeff4faa383ebb28e42d90e
Aaron <srimp12336691@gmail.com>
2024-01-17 12:18:11 +0800

    USER ENETITY

--- src/main/java/spring/mvc/model/entity/User.java
+++ src/main/java/spring/mvc/model/entity/User.java
@@ -1,10 +1,12 @@
 package spring.mvc.model.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 /*
  * 使用者資料
 +-------------------+-----------------
