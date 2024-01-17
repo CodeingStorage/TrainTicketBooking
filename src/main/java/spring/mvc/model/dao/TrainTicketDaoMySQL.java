116e2d9fea15b2f3b4cf339b9429be75159555bf
Aaron <srimp12336691@gmail.com>
2024-01-17 15:06:48 +0800

    DAO

--- src/main/java/spring/mvc/model/dao/TrainTicketDaoMySQL.java
+++ src/main/java/spring/mvc/model/dao/TrainTicketDaoMySQL.java
@@ -1,5 +1,62 @@
 package spring.mvc.model.dao;
 
-public class TrainTicketDaoMySQL {
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import spring.mvc.model.entity.Schedule;
+import spring.mvc.model.entity.Ticket;
+import spring.mvc.model.entity.User;
+
+@Repository
+public class TrainTicketDaoMySQL implements TrainTicketDao {
+
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addSchedule(Schedule schedule) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Boolean removeScheduleByTrainId(Integer trainId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean updateScheduleByTrainId(Integer trainId, String newSchedule) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addTicket(Ticket ticket) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Optional<Ticket> findTicketByUTicketId(Integer ticketId) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public Boolean updateTicketByUserIdAndTicketId(Integer userId, Integer ticketId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean removeTicketByUserIdAndTicketId(Integer userId, Integer ticketId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
